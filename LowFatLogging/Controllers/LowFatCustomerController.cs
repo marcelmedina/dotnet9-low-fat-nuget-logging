8f67e300e187f6e15a006290b683197a6defb08f
Marcel Medina <marcel.net@gmail.com>
2024-12-18 06:52:15 +1300

    dotnet9-low-fat-logging

---
+++ LowFatLogging/Controllers/LowFatCustomerController.cs
@@ -0,0 +1,31 @@
+﻿using LowFatLogging.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LowFatLogging.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LowFatCustomerController : ControllerBase
+    {
+        private ILogger<LowFatCustomerController> _logger;
+
+        public LowFatCustomerController(ILogger<LowFatCustomerController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]
+        public IActionResult CreateCustomer([FromBody] Customer customer)
+        {
+            if (customer is null)
+            {
+                _logger.LogWarning("Invalid customer creation attempt.");
+                return BadRequest("Customer data is invalid.");
+            }
+
+            _logger.LogInformation("Customer created: {@Customer}", customer);
+            return Ok("Customer created successfully.");
+        }
+    }
+}
---
+++ LowFatLogging/Models/Customer.cs
@@ -0,0 +1,9 @@
+﻿namespace LowFatLogging.Models
+{
+    public class Customer
+    {
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public int Age { get; set; }
+    }
+}
