d7e39ece6b3cf2a1039df2a721faa47f8b3553a7
Marcel Medina <marcel.net@gmail.com>
2024-12-21 05:08:27 +1300

    dotnet9-low-fat-logging

--- LowFatLogging/Program.cs
+++ LowFatLogging/Program.cs
@@ -1,3 +1,5 @@
+using Azure.Monitor.OpenTelemetry.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddProblemDetails();
+
+// Add OpenTelemetry and configure it to use Azure Monitor.
+builder.Services.AddOpenTelemetry().UseAzureMonitor();
 
 var app = builder.Build();
 
